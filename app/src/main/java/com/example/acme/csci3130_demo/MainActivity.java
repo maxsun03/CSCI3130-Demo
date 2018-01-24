8fa79aa7cc5bdb359a417e78504a3c2204031f0c
maxsun03 <max.sunkangshu@gmail.com>
2018-01-24 13:25:04 -0400

    new commit

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.acme.csci3130_demo.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.example.myFirstApp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
